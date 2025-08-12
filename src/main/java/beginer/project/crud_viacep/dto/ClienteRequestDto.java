fe151c163895e341b667f70049232dcf5f016a41
Celio <juniorcelioata@gmail.com>
2025-08-12 16:05:30 -0300

    Feat/insert constructor and getter and setter

--- src/main/java/beginer/project/crud_viacep/dto/ClienteRequestDto.java
+++ src/main/java/beginer/project/crud_viacep/dto/ClienteRequestDto.java
@@ -1,17 +1,37 @@
 package beginer.project.crud_viacep.dto;
 
 import beginer.project.crud_viacep.model.Viacep;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.sql.Timestamp;
 
-@Getter
-@Setter
 public class ClienteRequestDto {
 
     private String nome;
     private Timestamp ultimaDataModificada;
     private Viacep endereco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Timestamp getUltimaDataModificada() {
+        return ultimaDataModificada;
+    }
+
+    public void setUltimaDataModificada(Timestamp ultimaDataModificada) {
+        this.ultimaDataModificada = ultimaDataModificada;
+    }
+
+    public Viacep getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Viacep endereco) {
+        this.endereco = endereco;
+    }
 }
 
--- src/main/java/beginer/project/crud_viacep/dto/ClienteResponseDto.java
+++ src/main/java/beginer/project/crud_viacep/dto/ClienteResponseDto.java
@@ -1,17 +1,46 @@
 package beginer.project.crud_viacep.dto;
 
 import beginer.project.crud_viacep.model.Viacep;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.sql.Timestamp;
 
-@Getter
-@Setter
+
 public class ClienteResponseDto {
 
     private long id;
     private String nome;
     private Timestamp ultimaDataModificada;
     private Viacep endereco;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Timestamp getUltimaDataModificada() {
+        return ultimaDataModificada;
+    }
+
+    public void setUltimaDataModificada(Timestamp ultimaDataModificada) {
+        this.ultimaDataModificada = ultimaDataModificada;
+    }
+
+    public Viacep getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Viacep endereco) {
+        this.endereco = endereco;
+    }
 }
